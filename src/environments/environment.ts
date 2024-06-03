a5e138f683a98faf691ff83a073edecc0c344ce8
rammanoharvs <rammanohar.nair@gmail.com>
2024-06-03 12:43:54 +0000

    Code updated for the deployment id:

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   cookieExpiry: 60, ///in minutes
-  apiUrl: "http://localhost:5037/api/api/",
+  apiUrl: "https://7dcf-2401-4900-3305-3261-cda5-aee9-e1f5-6dcd.ngrok-free.app/api/api/",
 };
 
